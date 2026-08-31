90dbde913fb9fcb79bb9c670bfc37d0c1d70289d
Sollemdev <sollemdev@gmail.com>
2026-08-31 15:48:48 +0300

    Migrate Babel into modular directory structure (projects/babel/) with clean URLs and updated SEO/LLM manifests

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ const PROJECTS_DATA = [
     icon: "assets/icons/babel/icon48.png",
     banner: "assets/images/banners/Babel_Keyboard_Magic_1.jpg",
     github: "https://github.com/Sollemdev",
-    demo: "projects/babel.html",
+    demo: "projects/babel/",
     featured: true,
     highlights: [
       "Sub-300ms in-place DOM text replacement via Alt+T",
